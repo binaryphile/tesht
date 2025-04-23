92bd0bfa4ec17e9633e8dc93a20e0f9128b9b4d3
Ted Lilley <ted.lilley@digi.com>
2025-04-23 10:06:40 -0400

    refactor for testing

--- dummy_tests.bash
+++
@@ -1,9 +0,0 @@
-# A dummy test run by tesht_test.bash#test_tesht.testFile.
-# The point of this test is not to pass or fail but to simply run
-# two subtest cases so they may be counted in the results.
-test_RunTwoSubtests() {
-  subtest() { :; }                      # necessary, always passes
-  local -A case1=([name]=slug)          # name is required in this map
-  tesht.Run test_RunTwoSubTests case1   # it's ok to call tesht.Run with the same subtest twice
-  tesht.Run test_RunTwoSubTests case1
-}
--- tesht_test.bash
+++ tesht_test.bash
@@ -1,23 +1,34 @@
 # Because we are being run by tesht, it is already loaded and doesn't need to be sourced.
 NL=$'\n'
 
-# test_testFile tests that a test with two subtests is counted properly.
-test_tesht.testFile() {
+# test_tesht.test tests that the test function tests.
+test_tesht.test() {
   ## arrange
   timestamp() { return 0; }
+  tesht.Init timestamp
 
   ## act
   # run the command in a subshell and check counting happened
   local rc got
   got=$(
-    tesht.testFile timestamp dummy_tests.bash test_RunTwoSubtests
+    tesht.test testdummy_RunTwoSubtests
     exit $TestCountT
   ) && rc=$? || rc=$?
 
   ## assert
   # assert counting was done
   (( rc == 2 )) || {
-    echo "${NL}tesht.testFile: TestCountT is wrong. want: 2, got: $TestCountT$NL"
+    echo "${NL}TestCountT is wrong. want: 2, got: $TestCountT$NL"
     return 1
   }
+}
+
+# A dummy test run by test_tesht.test.
+# The point of this test is not to pass or fail but to simply run
+# two subtest cases so they may be counted in the results.
+testdummy_RunTwoSubtests() {
+  subtest() { :; }                      # necessary, always passes
+  local -A case=([name]=slug)           # name is required in this map
+  tesht.Run test_RunTwoSubTests case1   # it's ok to call tesht.Run with the same subtest twice
+  tesht.Run test_RunTwoSubTests case1
 }
